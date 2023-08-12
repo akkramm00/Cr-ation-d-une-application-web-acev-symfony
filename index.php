2a5a84058332923cfc6fb0211898faefb244728a
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-12 12:46:06 +0000

    add Symfony CLI

--- index.php
+++ index.php
@@ -21,5 +21,13 @@
       La dépendance "webapp" est un pack de composants à installer pour disposer de tous les outils nécessaires au développemnt d'une application web.0
       il exeiste d'autres packs officiels permettant d'etendre Symfony sur plein de sujets(ORM, debug, tests, etc). On peut retrouver ces packs sur le site de composer
     </p>
+
+
+    <h2>Création d'une application Symfony avec Symfony CLI</h2>
+
+  <p>
+    La création d'un projet Symfony via Symfony CLI se fait en deux temps . Tou d'abord, il faudra installer Symfony CLI sur notre machine. Ensuite , grace à ce programme, nous pourrons créer noàos applications Web.
+    Symfony CLI (Command Line Interface) est uin outil de développemnet qui nous aide à construire, exécuter et gérer nos applications Symfony directement depuis notre terminal. il est open source, fonctionne avec MacOs, Windows et Linux. Comme nous l'installons globalement à notre machine, nous ne devons l'installer qu'une seule fois sur notre système. cet outil nous simplifiera énormement le développement par la suite , donc il est fortement recommandé de l'installer.
+  </p>
   </body>
 </html>
